c1f566988225bbd85350fa535e903b9132d9861a
Monty1Crypto <117129720+Monty1Crypto@users.noreply.github.com>
2023-01-23 20:33:53 -0500

    fixed ip address

--- Project-001-Roman-Numerals-Converter/app.py
+++ Project-001-Roman-Numerals-Converter/app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 def convert(decimal_num):
     roman = {1000:'M', 900:'CM', 500:'D', 400:'CD', 100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
     num_to_roman = ''
-    
+
     for i in roman.keys():
         num_to_roman += roman[i]*(decimal_num//i)
         decimal_num %= i
@@ -29,4 +29,4 @@ def main_post():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    #app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
